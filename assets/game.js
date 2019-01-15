fbd26b52d14aef5c9d8b1c60afd0b3ca65e1a3b6
Dylon <Dielonh@gmail.com>
2019-01-14 22:49:40 -0600

    some stuff wasnt showing in my github

--- assets/game.js
+++ assets/game.js
@@ -1,57 +1,51 @@
-<script src="https://www.gstatic.com/firebasejs/5.7.3/firebase.js"></script>
-
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAYLZyMMVqAGId2oBUmoTWinz__9oTm3RQ",
     authDomain: "traintime-b609e.firebaseapp.com",
     databaseURL: "https://traintime-b609e.firebaseio.com",
     projectId: "traintime-b609e",
     storageBucket: "traintime-b609e.appspot.com",
     messagingSenderId: "978048435894"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#Submit").click(function (event) {
+$("#submit").click(function (event) {
     event.preventDefault();
-
-     var   name = $("#TrainName").val().trim();
-     var   Destination = $("#Destination").val().trim();
-     var   trainTime = $("#TrainTime").val().trim();
-     var   Frequency = $("#Frequency").val().trim();
-       // "NextArrival": firebase.database.ServerValue.TIMESTAMP,
-       // "Frequency": parseInt($("#Frequency").val().trim())
-
-
-var TrainArrival = {
-    name: name,
-    Destination: Destination,
-    trainTime: trainTime,
-    Frequency: Frequency,
-};
-
-database.ref().push(TrainArrival)
-
-    $("#trainName").val("");
-    $("#Destination").val("");
-    $("#TrainTime").val("");
-    $("#Frequency").val("");
+    database.ref().push({
+        "name": $("#TrainName").val().trim(),
+        "Destination": $("#Destination").val().trim(),
+        "TrainTime": $("#FirstTrainTime").val().trim(),
+        "NextTrain": firebase.database.ServerValue.TIMESTAMP,
+        "Frequency": parseInt($("#Frequency").val().trim())
+    });
 });
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-    console.log(childSnapshot.val());
-  
-    var name = childSnapshot.val().name;
-    var Destination = childSnapshot.val().Destination;
+database.ref()
+.orderByChild("TrainTime").limitToLast(100)
+.on("child_added", function (snapshot) {
     var Frequency = childSnapshot.val().Frequency;
-    var Train = moment(childSnapshot.val().Train, 'HH:mm').format('X');
-    var difference = moment().diff(moment.unix(Train), "minutes")
-    var timeLeft = moment().diff(moment.unix(Train), 'minutes') % Frequency
+    var difference = moment().diff(moment.unix(firstTrain), "minutes")
+    var timeLeft = moment().diff(moment.unix(firstTrain), 'minutes') % Frequency
+    var TrainTime = moment(childSnapshot.val().firstTrain, 'HH:mm').format('X');
     var min = moment(Frequency - timeLeft, 'mm').format('mm')
-    var nextTrain = moment().add(min, 'm').format('hh:mm A')
-
-    $("#Output1> tbody").append("<tr><td>" + name + "</td><td>" + Destination + "</td><td>" +
-    Frequency + "</td><td>" +nextTrain + "</td><td>" + min + "</td></tr>");
-  });
+    var NextTrain = moment().add(min, 'm').format('hh:mm A')
+    var TrainTime = moment(snapshot.val().TrainTime);
+    var added = moment(snapshot.val().NextTrain);
+    var datdiff = moment(added).diff(TrainTime);
+    console.log(moment(datdiff).format("MM"));
+    B = $("<thead>");
+    D = $("<tr>");
+    $("#output1").append(B);
+    $(B).append(D);
+    $(D).append("<td>" + snapshot.val().name);
+    $(D).append("<td>" + snapshot.val().Destination);
+    $(D).append("<td>" + snapshot.val().Frequency);
+    $(D).append("<td>" + snapshot.val().NextTrain);
+    $(D).append("<td>" + snapshot.val().min);
+  //  $(D).append("<button class='btn btn-danger deleteEmp' id='" + snapshot.val().dateAdded + "'>Delete")
+    $("#" + snapshot.val().dateAdded).click(function () {
+        var test = database.collection().doc().delete();
+    })
+});
